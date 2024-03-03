54541a0e923607468a04fdf89eb3de5c924a9e7a
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-03 19:39:58 +0545

    write transaction scheme

---
+++ server/express/src/models/transactions.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  drcr: {
+    type: String,
+    required: true,
+  },
+  party: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Party",
+  },
+  createdBy: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+transactionSchema.pre("save", function (next) {
+  this.updatedAt = new Date();
+  next();
+});
+
+transactionSchema.pre("findOneAndUpdate", function (next) {
+  this._update.updatedAt = new Date();
+  next();
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+export default Transaction;
