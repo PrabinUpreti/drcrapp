59152e01801c4b833effc15ac6f8277cc7f8b46f
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-04 23:02:34 +0545

    removed security

--- server/express/src/security/hash.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt";
-const hash = async (planeText) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashed = await bcrypt.hash(planeText, salt);
-  return hashed;
-};
