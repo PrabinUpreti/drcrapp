6ad1d19436c4ef289c331362138caf8b174167f0
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-05 22:35:21 +0545

    added /auth router

--- server/express/src/routes/routers.js
+++ server/express/src/routes/routers.js
@@ -4,9 +4,12 @@ const router = express.Router();
 import userRoutes from "./userRoutes.js";
 import partyRoutes from "./partyRoutes.js";
 import transactionRoutes from "./transactionRoutes.js";
+import { login } from "../middlewares/authMiddleware.js";
+import { loginValidation } from "../middlewares/validationMiddleware.js";
 
 router.use("/users", userRoutes);
 router.use("/parties", partyRoutes);
 router.use("/transactions", transactionRoutes);
+router.use("/auth", loginValidation, login);
 
 export default router;
