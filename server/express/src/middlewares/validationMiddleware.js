e7a4239a8be0f9db9c48e48861b73d7063ff3221
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-05 22:34:37 +0545

    login validation implementation

--- server/express/src/middlewares/validationMiddleware.js
+++ server/express/src/middlewares/validationMiddleware.js
@@ -11,3 +11,14 @@ export const userValidation = (req, res, next) => {
   if (validatedUser.error) res.json(validatedUser.error.details[0].message);
   else next();
 };
+
+const loginValidationScheme = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
+export const loginValidation = (req, res, next) => {
+  const validatedUser = loginValidationScheme.validate(req.body);
+  if (validatedUser.error) res.json(validatedUser.error.details[0].message);
+  else next();
+};
