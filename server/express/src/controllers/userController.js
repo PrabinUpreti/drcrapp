d0426b1db8ecf6f9b6ac3c6ad11c83fd7326602d
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-05 22:34:06 +0545

    Hide password from responce, user can register and get jwt token in heder

--- server/express/src/controllers/userController.js
+++ server/express/src/controllers/userController.js
@@ -1,9 +1,17 @@
 import User from "../models/users.js";
+import dotenv from "dotenv";
+import lodash from "lodash";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 
 export const getUsers = async () => {
   try {
     const users = await User.find();
-    return users;
+    const filteredUsers = users.map((user) => {
+      return lodash.pick(user, ["_id", "username", "email"]);
+    });
+    return filteredUsers;
   } catch (error) {
     console.error("Error fetching users:", error);
   }
@@ -12,21 +20,26 @@ export const getUsers = async () => {
 export const getUser = async (id) => {
   try {
     const user = await User.findById(id);
-    return user;
+    return lodash.pick(user, ["_id", "username", "email"]);
   } catch (error) {
     console.error("Error fetching users:", error);
   }
 };
 
-export const saveUser = async (param) => {
+export const saveUser = async (req, res, next) => {
   try {
+    const existUser = await User.findOne({ email: req.body.email });
+    if (existUser) return res.send("User already exist");
     const newUser = new User({
-      username: param.username,
-      email: param.email,
-      password: param.password,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
     });
     const savedUser = await newUser.save();
-    return savedUser;
+    const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_TOKEN);
+    res
+      .header("x-auth-toket", token)
+      .json(lodash.pick(savedUser, ["_id", "username", "email"]));
   } catch (error) {
     console.log(error);
     return "Existing information";
@@ -44,7 +57,8 @@ export const updateUser = async (id, param) => {
       },
       { new: true }
     );
-    if (updateUser) return updatedUser;
+    if (updateUser)
+      return lodash.pick(updatedUser, ["_id", "username", "email"]);
     else return "User Not Found";
   } catch (error) {
     console.log(error);
