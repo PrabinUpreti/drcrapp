a5e100e89f394ec432c916369de8e30b561889d3
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-12 16:34:49 +0545

    setup typescript, tailwind in project

---
+++ client/web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h6 className="text-center font-extrabold text-lg text-blue-950">
+      Hello World
+    </h6>
+  );
+}
+
+export default App;
