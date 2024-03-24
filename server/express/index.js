26ea21af4a7ebe05160b8ce2ed2fe82bf598513e
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-24 20:55:06 +0545

    added cors

--- server/express/index.js
+++ server/express/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { auth } from "./auth.js";
-dotenv.config();
+import cors from "cors";
+
+console.log("I am in server");
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+dotenv.config();
 app.use(express.json());
 
 app.use(auth);
