a8e7c7aa243e8af4b4485bfb8fedd4327b48af5f
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-24 20:57:00 +0545

    added username in jwt payload

--- server/express/src/models/users.js
+++ server/express/src/models/users.js
@@ -41,7 +41,7 @@ userSchema.pre("findOneAndUpdate", function (next) {
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
-    { _id: this._id, isAdmin: this.isAdmin },
+    { _id: this._id, isAdmin: this.isAdmin, username: this.username },
     process.env.JWT_TOKEN
   );
   return token;
