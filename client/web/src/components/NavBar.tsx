7bdfb64e7db9464089127701d3b0b6c270a413cf
Prabin Upreti <upretirajprabin@gmail.com>
2024-03-28 18:04:11 +0545

    added authentication in navlinks using auth context

--- client/web/src/components/NavBar.tsx
+++ client/web/src/components/NavBar.tsx
@@ -1,29 +1,41 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { decodeToken } from "react-jwt";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import { useAuth } from "../utils/auth";
 
 export const NavBar = () => {
   const [user, setUser] = useState("");
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const decodedtoken: any = decodeToken(token);
-      setUser(decodedtoken.username);
-    }
-  }, []);
+  const { credential } = useAuth();
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <NavLink to="/" className="text-white font-semibold text-lg">
-          Hello, {user}
+          DrCr App
         </NavLink>
         <div>
-          <NavLink to="/login" className="text-white mx-4">
-            Login
-          </NavLink>
-          <NavLink to="/logout" className="text-white mx-4">
-            Logout
-          </NavLink>
+          {credential.user ? (
+            <>
+              <NavLink to="/parties" className="text-white mx-4">
+                Parties
+              </NavLink>
+              <NavLink to="/transaction" className="text-white mx-4">
+                Transaction
+              </NavLink>
+              <NavLink to="/profile" className="text-white mx-4">
+                Hi {"  "}
+                {credential.user}
+              </NavLink>
+              <NavLink to="/logout" className="text-white mx-4">
+                Logout
+              </NavLink>
+            </>
+          ) : (
+            <NavLink to="/login" className="text-white mx-4">
+              Login
+            </NavLink>
+          )}
         </div>
       </div>
     </nav>
