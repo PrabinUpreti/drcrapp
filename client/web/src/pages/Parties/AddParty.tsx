ca570067e8885d5814aaeca7bc0eea99777fff32
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:19:05 +0545

    moved

---
+++ client/web/src/pages/Parties/AddParty.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import { validateParty } from "../../utils/validation";
+import { setPartyRequest } from "../../services/partiesService";
+import { useNavigate } from "react-router-dom";
+import { PartyForm } from "../../components/PartyForm";
+import { useStore } from "../../utils/store";
+
+export const AddParty = () => {
+  const {
+    focusName,
+    isDisable,
+    setIsDisable,
+    setError,
+    navigate,
+    party,
+    error,
+    setParty,
+    updatePartyState,
+  } = useStore();
+
+  useEffect(() => {
+    focusName.current.focus();
+  }, []);
+
+  const handleParty = async (e) => {
+    e.preventDefault();
+    setIsDisable(true);
+    const validParty = validateParty(party);
+    console.log(validParty);
+
+    if (!validParty) {
+      setIsDisable(false);
+      setError("Input nvalid, refill the form");
+      return;
+    }
+    setError(null);
+    const response = await setPartyRequest(party);
+    response.status === 200 ? updatePartyState(response.data) : "";
+    console.log(response);
+    navigate(-1);
+    setIsDisable(false);
+  };
+  return (
+    <PartyForm
+      type="addParty"
+      party={party}
+      error={error}
+      focusName={focusName}
+      handleParty={handleParty}
+      setParty={setParty}
+      isDisable={isDisable}
+    />
+  );
+};
