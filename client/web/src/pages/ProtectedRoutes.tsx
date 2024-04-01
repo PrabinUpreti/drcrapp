1c4a56d37ac3dd85437f6529f3530a4c8dabfab8
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:18:22 +0545

    added 404, renamed url name, etc

--- client/web/src/pages/ProtectedRoutes.tsx
+++ client/web/src/pages/ProtectedRoutes.tsx
@@ -1,28 +1,47 @@
-import { Routes, Route, Link } from "react-router-dom";
-import { Parties } from "../pages/Parties";
-import { Login } from "../pages/login";
-import { Logout } from "../pages/logout";
-import Transactions from "../pages/Transactions";
-import { AddParties } from "../pages/AddParties";
-import { AuthContext } from "../contexts/AuthContext";
 import { useContext, useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import { Transactions } from "./transactions/Transactions";
+import { Parties } from "./Parties/Parties";
+import { Logout } from "./Logout";
+import { Login } from "./Login";
+import { Profile } from "./Profile";
 import { Home } from "../pages/Home";
+import { AddParty } from "./Parties/AddParty";
 import { useAuth } from "../utils/auth";
+import { EditParty } from "./Parties/EditParty";
+import { DeleteParty } from "./Parties/DeleteParty";
+import { PageNotFound } from "./PageNotFound";
+import { AddTransaction } from "./transactions/AddTransaction";
+import { EditTransaction } from "./transactions/EditTransaction";
+import { DeleteTransaction } from "./transactions/DeleteTransaction";
 
 export const ProtectedRoutes = () => {
   const { credential } = useAuth();
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<PageNotFound />} />
       {credential.user ? (
         <>
-          <Route path="/parties" element={<Parties />}></Route>
-          <Route path="addParties" element={<AddParties />} />
+          <Route path="parties" element={<Parties />} />
+          <Route path="add_party" element={<AddParty />} />
+          <Route path="edit_party" element={<EditParty />} />
+          <Route path="delete_party" element={<DeleteParty />} />
+
           <Route path="transaction/:id" element={<Transactions />} />
+          <Route path="add_transaction" element={<AddTransaction />} />
+          <Route path="edit_transaction" element={<EditTransaction />} />
+          <Route path="delete_transaction" element={<DeleteTransaction />} />
+
+          <Route path="logout" element={<Logout />} />
+          <Route path="profile" element={<Profile />} />
           <Route path="logout" element={<Logout />} />
         </>
       ) : (
-        <Route path="login" element={<Login />} />
+        <>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="login" element={<Login />} />
+        </>
       )}
     </Routes>
   );
