f776e575ce05e7738d69539779332040a122d0d6
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:27:07 +0545

    added timestamps

--- server/express/src/models/users.js
+++ server/express/src/models/users.js
@@ -3,41 +3,44 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 dotenv.config();
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: Boolean,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: Boolean,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
-userSchema.pre("save", function (next) {
-  this.updatedAt = new Date();
-  next();
-});
+// userSchema.pre("save", function (next) {
+//   this.updatedAt = new Date();
+//   next();
+// });
 
-userSchema.pre("findOneAndUpdate", function (next) {
-  this._update.updatedAt = new Date();
-  next();
-});
+// userSchema.pre("findOneAndUpdate", function (next) {
+//   this._update.updatedAt = new Date();
+//   next();
+// });
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
