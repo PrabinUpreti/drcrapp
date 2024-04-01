75228e428c3402ceb23555c81de2c4ea0277f15d
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:15:29 +0545

    capitalized

--- client/web/src/pages/Logout.tsx
+++ client/web/src/pages/Logout.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import { useAuth } from "../utils/auth";
+import { useStore } from "../utils/store";
 
 export const Logout = () => {
+  const { parties, setParties, navigate, getDrSum, getCrSum } = useStore();
+
   const { logout } = useAuth();
   useEffect(() => {
+    setParties([]);
     logout();
   }, []);
   return null;
