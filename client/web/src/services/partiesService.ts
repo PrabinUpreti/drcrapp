63086864750452c01e72cb94b9e79160707ce873
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:21:39 +0545

    modified getparty url, added setPartyRequest, updatePartyRequest, updatePartyAmountRequest

--- client/web/src/services/partiesService.ts
+++ client/web/src/services/partiesService.ts
@@ -5,11 +5,99 @@ const result = {
 };
 export const getParties = async () => {
   try {
-    const response = await axios.get("http://localhost:8000/api/parties", {
+    const response = await axios.get("http://localhost:8000/api/userParties", {
       headers: {
         "x-auth-token": localStorage.getItem("token"),
       },
     });
+
+    return { ...result, status: response.status, data: response.data };
+  } catch (error: any) {
+    console.error("Login error:" + error.response.data);
+    return {
+      ...result,
+      status: error.response.status,
+      data: error.response.data,
+    };
+  }
+};
+
+export const setPartyRequest = async ({ name, phone, address, photo }) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:8000/api/parties",
+      {
+        name,
+        phone,
+        photo,
+        address,
+      },
+      {
+        headers: {
+          "x-auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
+
+    return { ...result, status: response.status, data: response.data };
+  } catch (error: any) {
+    console.error("Login error:" + error.response.data);
+    return {
+      ...result,
+      status: error.response.status,
+      data: error.response.data,
+    };
+  }
+};
+
+export const updatePartyRequest = async ({
+  name,
+  phone,
+  address,
+  photo,
+  id,
+}) => {
+  try {
+    const response = await axios.put(
+      `http://localhost:8000/api/parties/${id}`,
+      {
+        name,
+        phone,
+        photo,
+        address,
+      },
+      {
+        headers: {
+          "x-auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
+
+    return { ...result, status: response.status, data: response.data };
+  } catch (error: any) {
+    console.error("Login error:" + error.response.data);
+    return {
+      ...result,
+      status: error.response.status,
+      data: error.response.data,
+    };
+  }
+};
+
+export const updatePartyAmountRequest = async ({ amount, drcr, party }) => {
+  try {
+    const response = await axios.put(
+      `http://localhost:8000/api/userPartiesAmount/${party}`,
+      {
+        amount,
+        drcr,
+      },
+      {
+        headers: {
+          "x-auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
 
     return { ...result, status: response.status, data: response.data };
   } catch (error: any) {
