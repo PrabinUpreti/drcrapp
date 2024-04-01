b7a48f112f1cab8d666ebeb60c6264ff6b9894ca
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-01 23:28:34 +0545

    created new routers , userparty, userparties amount

--- server/express/src/routes/routers.js
+++ server/express/src/routes/routers.js
@@ -8,11 +8,16 @@ import { login } from "../middlewares/authMiddleware.js";
 import { loginValidation } from "../middlewares/validationMiddleware.js";
 import { authorize } from "../middlewares/authorizationMiddleware.js";
 import transactionOfPartyRoutes from "./transactionOfPartyRoutes.js";
+import userPartyRoutes from "./userPartyRoutes.js";
+import userPartyAmountRoutes from "./userPartyAmountRoutes.js";
 router.post("/auth", loginValidation, login);
 router.use(authorize);
-router.use("/users", userRoutes);
-router.use("/parties", partyRoutes);
-router.use("/transactions", transactionRoutes);
-router.use("/transactionOfParty", transactionOfPartyRoutes);
+router.use("/users", userRoutes); // admin all access
+router.use("/parties", partyRoutes); // admin all access
+router.use("/userParties", userPartyRoutes); // user
+router.use("/userPartiesAmount", userPartyAmountRoutes); // user
+
+router.use("/transactions", transactionRoutes); // admin all access
+router.use("/transactionOfParty", transactionOfPartyRoutes); // user, party
 
 export default router;
