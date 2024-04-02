a1fa0716091bed292873bb7d63a21b4fb6e95668
Prabin Upreti <upretirajprabin@gmail.com>
2024-04-02 20:04:21 +0545

    styled changed

--- client/web/src/pages/Parties/Parties.tsx
+++ client/web/src/pages/Parties/Parties.tsx
@@ -30,7 +30,7 @@ export const Parties = () => {
   };
 
   return (
-    <div className="w-full  p-4 bg-white border border-gray-200 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full p-4 bg-white border border-gray-200 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
       <div className="flex justify-between mb-5">
         <div className="flex items-center justify-between mb-4">
           <h5 className="text-3xl font-bold uppercase leading-none text-gray-900 dark:text-white">
@@ -49,21 +49,21 @@ export const Parties = () => {
       <div className="flow-root">
         <ul
           role="list"
-          className="divide-y flex flex-wrap lg:justify-between sm:justify-around  divide-gray-200 dark:divide-gray-700"
+          className="divide-y flex flex-wrap justify-center max-w-full divide-gray-200 dark:divide-gray-700"
         >
           {parties.map((item, index) => {
             return (
               <React.Fragment key={index}>
-                <li className="max-w-lg">
-                  <div className="py-5 sm:space-x-5 space-x-40 flex justify-between cursor-pointer">
+                <li className="max-w-full">
+                  <div className="py-5 space-x-3 lg:space-x-5 flex justify-between cursor-pointer">
                     <div
                       onClick={() => {
                         partyDetails(item._id, item);
                       }}
-                      className="max-h-36 min-w-36 "
+                      className="max-h-24 min-w-24 self-center "
                     >
                       <img
-                        className="max-w-36 max-h-36 rounded-xl"
+                        className="h-24 rounded-xl"
                         src={item.photo}
                         alt={item.name}
                       />
@@ -72,7 +72,7 @@ export const Parties = () => {
                       onClick={() => {
                         partyDetails(item._id, item);
                       }}
-                      className=" flex flex-col sm:w-96 max-w-48 space-y-1"
+                      className=" flex flex-col min-w-40 justify-center space-y-1"
                     >
                       <p className="text-xl font-bold text-gray-900 truncate dark:text-white">
                         {item.name}
@@ -105,7 +105,7 @@ export const Parties = () => {
                         </span>
                       </p>
                     </div>
-                    <div className="flex-col  align-middle text-base font-semibold text-gray-900 dark:text-white">
+                    <div className=" flex flex-col justify-center max-w-32 space-y-3 text-base font-semibold text-gray-900 dark:text-white">
                       <div>
                         <p className="flex">
                           <MdPhone className="size-6" />
